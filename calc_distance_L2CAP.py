10e4d3474b3674c18397d4d327d2ab758842e86b
makoch <makoch@seemoo.tu-darmstadt.de>
2023-05-13 09:39:40 +0200

    new data and cleanup

--- calc_distance_L2CAP.py
+++ calc_distance_L2CAP.py
@@ -1,41 +1,46 @@
 import sys
+import statistics
+import binascii
 SPEED_OF_LIGHT = 299792458
 
-
-def calc_timestamp_dec(hex_string: str):
-    hex_bytes = bytes.fromhex(hex_string)
-    swap_endianness = bytes(reversed(hex_bytes)).hex()
-    dec = int(swap_endianness, 16)
-    return dec
-
-def wtf(param):
+def unit_conversion(param):
   return param / 998400000000.0
 
-def single_sided_ranging(values: list):
-    t_round = values[0]
-    t_reply = values[1]
+def multiply_by_c(param):
+  return param * 299792458.0
 
-    tof = 0.5 * (t_round - t_reply)
-    range = wtf(tof) * SPEED_OF_LIGHT
-    return range
+def single_sided_ranging(t_round, t_reply):
+    tof = unit_conversion(0.5 * (t_round - t_reply))
+    range = multiply_by_c(tof)
+    return tof, range
 
-
-# def double_sided_ranging(values: list):
-#     t_round_1 = values[0]
-#     t_reply_1 = values[1]
-#     t_round_2 = values[2]
-#     t_reply_2 = values[3]
-
-#     res = (t_round_1 * t_round_2 - t_reply_1 * t_reply_2) / (t_round_1 + t_round_2 + t_reply_1 + t_reply_2) * 10 ** -12 * SPEED_OF_LIGHT
-
-#     return res
-
+def extract_timestamps(hex_str):
+    bytes_data = binascii.unhexlify(hex_str.replace(" ", ""))
+    t_round_bytes = bytes_data[9:13][::-1]
+    t_reply_bytes = bytes_data[17:21][::-1]
+    t_round_hex = binascii.hexlify(t_round_bytes).decode('utf-8')
+    t_reply_hex = binascii.hexlify(t_reply_bytes).decode('utf-8')
+    t_round = int(t_round_hex, 16)
+    t_reply = int(t_reply_hex, 16)
+    return [t_round, t_reply]
 
 if __name__ == '__main__':
-    with open(sys.argv[1]) as file:
-        lines = [line.rstrip().replace(' ', '') for line in file]
-        lines = list(filter(lambda line: len(line) == 54, lines))
-        lines = [[calc_timestamp_dec(line[18:26]), calc_timestamp_dec(line[34:42])] for line in lines]
 
-        for line in lines:
-            print(single_sided_ranging(line))
+    with open(sys.argv[1]) as file:
+        lines = [line.rstrip() for line in file]
+        lines = list(filter(lambda x: ("91 15 24" in x), lines))
+        timestamps = [extract_timestamps(line) for line in lines]
+        timestamps = list(filter(lambda values: values != [0, 0], timestamps))
+
+        ranges = []
+
+        for values in timestamps:
+            t_round = values[0]
+            t_reply = values[1]
+            tof, range = single_sided_ranging(t_round=t_round, t_reply=t_reply)
+            ranges.append(range)
+
+        print(f"Mean: {statistics.mean(ranges)}")
+        print(f"Median: {statistics.median(ranges)}")
+        print(f"Max: {max(ranges)}")
+        print(f"Min: {min(ranges)}")
